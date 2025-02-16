3063cca62ebacb8853064edce877127eed6296b7
Joel Braid Feasey <jbraid38@gmail.com>
2025-02-16 13:15:07 +0000

    added bulma style framework and worked on navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 
-
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
